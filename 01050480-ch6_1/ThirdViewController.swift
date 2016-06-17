5e422d47a2d552ab0fba7127ddd12912a49901a2
a9527316 <a9527316@gmail.com>
2016-06-17 21:42:26 +0800

    first project

---
+++ 01050480-ch6_1/ThirdViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ThirdViewController.swift
+//  01050480-ch6_1
+//
+//  Created by Stu_17 on 2016/3/28.
+//  Copyright © 2016年 Stu_17. All rights reserved.
+//
+
+//import Foundation
+import UIKit
+
+class ThirdViewController: UIViewController{
+    
+    override func viewDidLoad(){
+        super.viewDidLoad()
+        self.view.backgroundColor=UIColor.greenColor()
+    }
+    
+    override func didReceiveMemoryWarning(){
+        super.didReceiveMemoryWarning()
+    }
+}
